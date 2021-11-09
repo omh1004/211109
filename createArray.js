93b20cd51405c0d22ab34686a38e9e9d754fcbde
omh1004 <ohchef2020@gmail.com>
2021-11-09 18:46:58 +0900

    array add, remove

---
+++ createArray.js
@@ -0,0 +1,43 @@
+
+//배열을 생성
+const arr = [`사과`,`바나나`]
+
+console.log(arr)
+console.log(arr.length)
+
+//배열요소을 추가
+arr.push(`사과`)
+
+console.log(arr)
+console.log(arr.length)
+
+//배열요소 제거
+//배열의 끝에서 부터 항목 제거
+arr.pop()
+arr.pop()
+
+console.log(arr)
+console.log(arr.length)
+
+arr.push(`바나나`,'파인애플','파인애플')
+
+console.log(arr)
+console.log(arr.length)
+
+//배열의 앞에서 항목 제거하기
+arr.shift()
+
+console.log(arr)
+console.log(arr.length)
+
+//배열의 앞에서 항목 추가하기
+arr.unshift(`구아바`,`메론`)
+
+console.log(arr)
+console.log(arr.length)
+
+
+
+
+
+
